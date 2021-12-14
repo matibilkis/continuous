62aad9a1166a6d38de4f9e3b4e813b7bc180da31
matibilkis <matiasbilkis@gmail.com>
2021-12-14 21:14:48 +0100

    a

--- .ipynb_checkpoints/subash-checkpoint.sh
+++ .ipynb_checkpoints/subash-checkpoint.sh
@@ -4,5 +4,10 @@ periods=$2
 cd ~/continuous
 . ~/qenv_bilkis/bin/activate
 indextraj=$(ls -1q | wc -l)
+echo $ppp
+echo ppp
+echo $periods
+echo indextraj
+echo $indextraj
 python3 main.py --ppp ppp --periods periods --itraj indextraj
 deactivate
