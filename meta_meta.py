a2d5ea0b265a5ca2f4d6a0f7c80c9eb02dda4612
matibilkis <matiasbilkis@gmail.com>
2022-02-16 18:52:23 +0100

    pull kalman

--- meta_meta.py
+++ meta_meta.py
@@ -7,26 +7,24 @@ from misc import *
 params = give_def_params() #params = [eta, gamma, kappa, omega, n]
 
 windows = np.concatenate([(10**k)*np.arange(2,3) for k in range(1)])
-windows = []
 save_windows(windows)
 method = "rossler"
-ppp = 1000
-periods = 5000
+ppp = 10000
+periods = 1000
 rppp = 1
 rppp_reference = 1
 
 
 only_plot = 0
-
 only_traj=0
 no_kalman = 1
 #integrate time trace with different steps
 
 
 if only_plot != 1:
-    # for rppp in [rppp] + list(windows):
-    #     print("INTEGRATING TIME-TRACE, stroboscopic factor  x{}".format(rppp))
-    #     os.system("python3 integrate.py --ppp {} --periods {} --rppp {} --method {} --params {}".format(ppp, periods, rppp, method,  params_to_string(params)))
+    for rppp in [rppp] + list(windows):
+        print("INTEGRATING TIME-TRACE, stroboscopic factor  x{}".format(rppp))
+        os.system("python3 integrate.py --ppp {} --periods {} --rppp {} --method {} --params {}".format(ppp, periods, rppp, method,  params_to_string(params)))
 
     if only_traj != 1:
         # how does the landscape looks for the big time trace?
