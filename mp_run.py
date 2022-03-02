47e3629de9eade2259af1b0d9b344330bf95324e
matiasbilkis <matiasbilkis@gmail.com>
2022-03-02 14:39:51 +0100

    apparently better

---
+++ mp_run.py
@@ -0,0 +1,17 @@
+import os
+import multiprocessing as mp
+from numerics.utilities.misc import *
+params = give_def_params() #params = [eta, gamma, kappa, omega, n]
+
+method = "rossler"
+ppp = 1000
+periods = 10
+
+cores = mp.cpu_count()
+
+def int_seed(seed, ppp=1000, periods=10, method="rossler"):
+    os.system("python3 numerics/integration/integrate_with_fisher.py --ppp {} --periods {} --method {} --params {} --itraj {}".format(ppp, periods, method,  params_to_string(params), seed))### default rppp = 1
+
+jobs = 20
+with mp.Pool(cores-1) as p:
+    p.map(int_seed, range(1,jobs))
