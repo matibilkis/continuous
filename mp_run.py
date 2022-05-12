df14f9941b910fe7cb3a4b0c441a55f045578eeb
giq <giq@golem.uab.cat>
2022-05-12 20:39:42 +0200

    checking histograms likelihoods

--- mp_run.py
+++ mp_run.py
@@ -1,3 +1,4 @@
+
 import os
 import multiprocessing as mp
 from numerics.utilities.misc import *
@@ -5,9 +6,9 @@ from numerics.utilities.misc import *
 
 cores = mp.cpu_count()
 
-mode="frequencies"
-dt = 1e-4
-total_time = 20.
+mode="damping"
+dt = 1e-6
+total_time = 4.
 ppp=5*1e3
 def int_seed(seed):
     for k in range(10):
@@ -17,4 +18,27 @@ def int_seed(seed):
 
 
 with mp.Pool(cores-2) as p:
-    p.map(int_seed, range(1,500, 10))
+    p.map(int_seed, range(1,1000, 10))
+
+
+
+# import os
+# import multiprocessing as mp
+# from numerics.utilities.misc import *
+#
+#
+# cores = mp.cpu_count()
+#
+# mode="frequencies"
+# dt = 1e-4
+# total_time = 20.
+# ppp=5*1e3
+# def int_seed(seed):
+#     for k in range(10):
+#         os.system("python3 numerics/integration/integrate.py --itraj {} --mode {} --dt {} --total_time {} --ppp {}".format(seed+k, mode, dt, total_time, ppp))
+#         os.system("python3 numerics/integration/integrate.py --itraj {} --flip_params 1 --mode {} --dt {} --total_time {} --ppp {}".format(seed+k, mode, dt, total_time, ppp))
+#         print(f"{k}, {seed}, done")
+#
+#
+# with mp.Pool(cores-2) as p:
+#     p.map(int_seed, range(1,500, 10))
